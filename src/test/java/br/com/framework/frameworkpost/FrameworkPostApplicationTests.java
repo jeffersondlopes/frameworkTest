7ff2b130248807e8c94790f54c34a49716250e00
jefferson <jefferson.diego.e@riachuelo.com.br>
2022-03-21 16:26:07 -0300

    First Commit.

---
+++ src/test/java/br/com/framework/frameworkpost/FrameworkPostApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.framework.frameworkpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
