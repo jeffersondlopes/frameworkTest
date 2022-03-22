cf4c5b3b0c45ff63b2e9356dfef5322233b7cd1a
jefferson <jefferson.diego.e@riachuelo.com.br>
2022-03-21 22:15:08 -0300

    Add. domain commentPost.

--- src/main/java/br/com/framework/frameworkpost/domain/CommentsPosts.java
+++ src/main/java/br/com/framework/frameworkpost/domain/CommentsPosts.java
@@ -5,17 +5,30 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.tomcat.jni.Local;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "comments_posts", indexes = {
-        @Index(columnList = "post_id", name = "post_id_idx")
-})
+        @Index(columnList = "post_id", name = "comment_post_id_idx"),
+        @Index(columnList = "email,name", name = "comment_email_name_idx")
+        })
 @SequenceGenerator(name = "user_id_seq", allocationSize = 1)
 public class CommentsPosts {
 
@@ -24,9 +37,12 @@ public class CommentsPosts {
     @Column(updatable = false)
     private Long id;
 
+    private String email;
+
+    private String name;
+
     @Column(name = "date_created")
-    @JsonFormat(shape = JsonFormat.Shape.STRING,
-            pattern = "dd-MM-yyyy hh:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime dateCreate;
 
     @PrePersist
