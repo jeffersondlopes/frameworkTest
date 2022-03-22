5c3766de55cde7d6b5c801c5f6b670e09d3733f7
jefferson <jefferson.diego.e@riachuelo.com.br>
2022-03-22 15:51:22 -0300

    Change structture file storage service.

--- src/main/java/br/com/framework/frameworkpost/service/PhotoPostService.java
+++ src/main/java/br/com/framework/frameworkpost/service/PhotoPostService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class PhotoPostService {
@@ -32,13 +33,18 @@ public class PhotoPostService {
         Post post = postsService.findById(postId);
         PhotosPost photosPost = buildPhotoPost(photoPostInput);
         photosPost.setPost(post);
-        FileToStorage fileToStorage = buildFileStorage(photoPostInput);
+        FileToStorage fileToStorage = null;
+        try {
+            fileToStorage = buildFileStorage(photoPostInput.getFile().getInputStream(), photosPost.getNameFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         photStorageService.savePhoto(fileToStorage);
         return photoPostRepository.save(photosPost);
 
     }
 
-    private PhotosPost buildPhotoPost(PhotoPostInput photoPostInput){
+    private PhotosPost buildPhotoPost(PhotoPostInput photoPostInput) {
         return PhotosPost.builder()
                 .description(photoPostInput.getDescription())
                 .nameFile(photStorageService.generateNameFile(photoPostInput.getFile().getOriginalFilename()))
@@ -47,22 +53,19 @@ public class PhotoPostService {
                 .build();
     }
 
-    private FileToStorage buildFileStorage(PhotoPostInput photoPostInput) {
-        try {
-            return FileStorageService.FileToStorage.builder()
-                    .nomeAquivo(photStorageService.generateNameFile(photoPostInput.getFile().getOriginalFilename()))
-                    .inputStream(photoPostInput.getFile().getInputStream())
-                    .build();
-        } catch (IOException e) {
-            throw new RuntimeException("Erro ao gerar o FileStorage.",e);
-        }
+    private FileToStorage buildFileStorage(InputStream inputStream, String fileName) {
+        return FileStorageService.FileToStorage.builder()
+                .nomeAquivo(fileName)
+                .inputStream(inputStream)
+                .build();
     }
 
     @Transactional
     public void delete(Long postId, String fileName) {
         PhotosPost photosPost = photoPostRepository.findByPostIdAndNameFile(postId, fileName)
                 .orElseThrow(() -> new RuntimeException("Arquivo Não encontrado"));
-        photoPostRepository.delete(photosPost);
+         photoPostRepository.delete(photosPost);
+        photStorageService.remover(fileName);
     }
 
 }
