ac64f10ce3bde827fce5ace473b87678a17d6553
jefferson <jefferson.diego.e@riachuelo.com.br>
2022-03-25 12:12:49 -0300

    Config ResourceServerConfig.java

--- src/main/java/br/com/framework/frameworkpost/security/ResourceServerConfig.java
+++ src/main/java/br/com/framework/frameworkpost/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package br.com.framework.frameworkpost.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -12,8 +17,17 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .anyRequest().authenticated()
+                .anyRequest().authenticated()
             .and()
-            .oauth2ResourceServer().opaqueToken();
+                .cors().and()
+                .oauth2ResourceServer()
+                .jwt();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s".getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+
 }
